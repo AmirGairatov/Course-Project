059ae2836521c73f0c50f4579a61d2404741168b
AmirGairatov <amircagay@gmail.com>
2025-10-12 14:51:14 +0500

    Update BasicEnemyAnimInstance.cpp

--- CodeForBasicEnemy/BasicEnemyAnimInstance.cpp
+++ CodeForBasicEnemy/BasicEnemyAnimInstance.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "BasicEnemyAnimInstance.h"
 #include "BasicEnemy.h"
 
@@ -34,4 +31,5 @@ void UBasicEnemyAnimInstance::OnStateAnimationEnds()
 		}
 		
 	}
+
 }
