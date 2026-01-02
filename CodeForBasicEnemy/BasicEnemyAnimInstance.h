fd0324f37a7108da406dd1933957ef6410e9722c
AmirGairatov <amircagay@gmail.com>
2026-01-03 00:26:09 +0500

    Update BasicEnemyAnimInstance.h

--- CodeForBasicEnemy/BasicEnemyAnimInstance.h
+++ CodeForBasicEnemy/BasicEnemyAnimInstance.h
@@ -1,3 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
@@ -35,5 +37,7 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void OnStateAnimationEnds();
-};
 
+	UFUNCTION(BlueprintCallable)
+	void DamageDealControl(bool wtf);
+};
