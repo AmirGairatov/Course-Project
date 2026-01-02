03c09bc00bc175bbe70bb2eaddc37f7c52016068
AmirGairatov <amircagay@gmail.com>
2026-01-03 00:25:34 +0500

    Update BasicEnemyController.h

--- CodeForBasicEnemy/BasicEnemyController.h
+++ CodeForBasicEnemy/BasicEnemyController.h
@@ -1,3 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
@@ -13,5 +15,10 @@ class COURSEPROJECT_API ABasicEnemyController : public AAIController
 	GENERATED_BODY()
 public:
 	void MakeAttackDecision(APawn* targetPawn);
-};
+	void StrafeAroundTarget(APawn* TargetPawn, float StrafeRadius = 300.0f, float StrafeSpeed = 90.0f);
+private:
+	float CurrentStrafeAngle = 0.0f;
+	bool bStrafeDirectionRight = true;
+	bool bStrafeDirectionRightInitialized = false;
 
+};
