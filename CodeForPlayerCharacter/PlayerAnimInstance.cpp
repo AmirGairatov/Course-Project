270865e2dd5aba231ebf163bce122198c3b3703b
AmirGairatov <amircagay@gmail.com>
2026-01-03 00:24:48 +0500

    Update PlayerAnimInstance.cpp

--- CodeForPlayerCharacter/PlayerAnimInstance.cpp
+++ CodeForPlayerCharacter/PlayerAnimInstance.cpp
@@ -1,3 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "PlayerAnimInstance.h"
 #include "PlayerCharacter.h"
 
@@ -5,9 +8,42 @@ void UPlayerAnimInstance::OnStateAnimationEnds()
 {
 	if (State == EPlayerState::Heal)
 	{
+		bool bIsHealing = false;
 		State = EPlayerState::Locomotion;
 	}
+	if (State == EPlayerState::Land)
+	{
+		UE_LOG(LogTemp, Error, TEXT("FallRecover"));
+		State = EPlayerState::FallRecovery;
+	}
+	else if (State == EPlayerState::FallRecovery)
+		{
+		UE_LOG(LogTemp, Error, TEXT("FallRecoverEnd"));
+		State = EPlayerState::Locomotion;
+		auto ownerActor = this->GetOwningActor();
+		auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+		if (playerCharacter)
+		{
+			playerCharacter->bIsInFallRecover = false;
+			playerCharacter->bIsAttacking = false;
+		}
+
+		}
 	if (State == EPlayerState::Roll)
+	{
+		State = EPlayerState::Locomotion;
+	}
+	else if (State == EPlayerState::RespawnBuff)
+	{
+		auto ownerActor = this->GetOwningActor();
+		auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+		if (playerCharacter)
+		{
+			playerCharacter->bIsInRespawnProcess = false;
+		}
+		State = EPlayerState::Locomotion;
+	}
+	else if (State == EPlayerState::SpecialAttack)
 	{
 		State = EPlayerState::Locomotion;
 	}
@@ -23,6 +59,7 @@ void UPlayerAnimInstance::OnStateAnimationEnds()
 		{
 			State = EPlayerState::Locomotion;
 		}
+		playerCharacter->bIsHit = false;
 	}
 	else if (State == EPlayerState::SecondAttack)
 	{
@@ -36,10 +73,39 @@ void UPlayerAnimInstance::OnStateAnimationEnds()
 		{
 			State = EPlayerState::Locomotion;
 		}
+		playerCharacter->bIsHit = false;
 	}
 	else if (State == EPlayerState::ThirdAttack)
 	{
+		auto ownerActor = this->GetOwningActor();
+		auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+
 		State = EPlayerState::Locomotion;
+		if (playerCharacter)
+		{
+			playerCharacter->bIsHit = false;
+		}
+	}
+	else if (State == EPlayerState::Spawn)
+	{
+		auto ownerActor = this->GetOwningActor();
+		auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+		if (playerCharacter)
+		{
+			playerCharacter->bIsAttacking = false;
+		}
+		State = EPlayerState::Locomotion;
+	}
+	else if (State == EPlayerState::RecoverFromHeavyHit)
+	{
+		auto ownerActor = this->GetOwningActor();
+		auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+		State = EPlayerState::Locomotion;
+		if (playerCharacter != nullptr)
+		{
+			playerCharacter->bIsHit = false;
+			playerCharacter->bIsInHitAnimation = false;
+		}
 	}
 	else
 	{
@@ -57,13 +123,43 @@ void UPlayerAnimInstance::OnStateAnimationEnds()
 			}
 			else
 			{
-				State = EPlayerState::Die;
+				State = EPlayerState::Locomotion;
 			}
+			playerCharacter->bIsHit = false;
+			playerCharacter->bIsInHitAnimation = false;
 		}
-		else if (State == EPlayerState::Die)
+		else if (State == EPlayerState::HeavHit)
 		{
-			//...
+			State = EPlayerState::RecoverFromHeavyHit;
 		}
 	}
 }
 
+void UPlayerAnimInstance::OnWeaponSocketChanged()
+{
+	auto ownerActor = this->GetOwningActor();
+	auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+	if (playerCharacter)
+	{
+		playerCharacter->ChangeWeaponSocket();
+	}
+}
+void UPlayerAnimInstance::OnDeathWidgetShown()
+{
+	auto ownerActor = this->GetOwningActor();
+	auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+	if (playerCharacter)
+	{
+		playerCharacter->ShowYouDiedWidget();
+	}
+}
+
+void UPlayerAnimInstance::Boom()
+{
+	auto ownerActor = this->GetOwningActor();
+	auto playerCharacter = Cast<APlayerCharacter>(ownerActor);
+	if (playerCharacter)
+	{
+		playerCharacter->RespawnBuff();
+	}
+}
