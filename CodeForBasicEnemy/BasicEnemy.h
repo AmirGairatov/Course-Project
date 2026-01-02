7fe20d5f7d092fb08bbcd284b74ac7fe085e6efd
AmirGairatov <amircagay@gmail.com>
2026-01-03 00:26:51 +0500

    Update BasicEnemy.h

--- CodeForBasicEnemy/BasicEnemy.h
+++ CodeForBasicEnemy/BasicEnemy.h
@@ -1,9 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "EnemyHealthBar.h"
+#include "Components/CapsuleComponent.h"
 #include "NiagaraSystem.h"
+#include "Sound/SoundCue.h"
 #include "BasicEnemy.generated.h"
 
 UCLASS()
@@ -16,15 +20,18 @@ public:
 	ABasicEnemy();
 
 	UPROPERTY(EditAnywhere, Category="Enemy Params")
-	int HealthPoints = 5000;
+	int HealthPoints = 100;
 
 	UPROPERTY(EditAnywhere, Category = "Enemy Params")
-	float AttackInterval = 2.7f;
-	UPROPERTY(EditAnywhere, Category = "Enemy Params")
-	float AttackUPInterval = 2.5f;
+	int BasicDamage = 100;
 
 	UPROPERTY(EditAnywhere, Category = "Enemy Params")
-	float HeavyAttackInterval = 3.1f;
+	float AttackInterval = 2.15f;
+	UPROPERTY(EditAnywhere, Category = "Enemy Params")
+	float AttackUPInterval = 2.16f;
+
+	UPROPERTY(EditAnywhere, Category = "Enemy Params")
+	float HeavyAttackInterval = 2.23f;
 
 	UPROPERTY(EditAnywhere, Category = "Enemy Params")
 	float HitAnimInterval = 1.0f;
@@ -32,21 +39,64 @@ public:
 	UPROPERTY(EditAnywhere, Category = "Enemy Params")
 	int AttackRange = 200.0f;
 
+	UPROPERTY(EditAnywhere, Category = "Enemy Params")
+	float AttackRecoverInterval = 1.5f;
+
+	float _AttackRecoverCountingDown;
+
+	bool bIsInAttackRecover = false;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	float LoseSightDelay = 2.0f;
+
+	UFUNCTION()
+	void OnLosePlayer();
+
+	float LostSightTime = 0.0f;
+	FTimerHandle LoseSightTimerHandle;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+	UCapsuleComponent* LeftFistCapsule;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat")
+	UCapsuleComponent* RightFistCapsule;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX")
+	UNiagaraSystem* BuffEffectNiagara;
+
 	int _HealthPoints;
 	float _AttackCountingDown;
 	float _HitAnimCountingDown;
-	bool bIsAttacking = false;
-	bool bAreHit = false;
-	APawn* _ChasedTarget = nullptr;
+	bool bIsHeavyAttacking = false;
+	bool bIsDamageDealed = false;
 
+	UFUNCTION()
+	void OnFistBeginOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundCue* MySoundCue;
+
+	UPROPERTY(EditAnywhere, Category = "Audio")
+	USoundCue* PunchCue;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	bool bAreHit = true;
+	bool bIsAttacking = false;
+	bool bIsBuffed = false;
+	APawn* _ChasedTarget = nullptr;
 	UFUNCTION(BlueprintCallable, Category = "Enemy", meta = (DisplayName="Get HP"))
 	int GetHealthPoints();
 	UFUNCTION(BlueprintCallable, Category = "Enemy")
@@ -71,14 +121,20 @@ public:
 	void Hit(int damage);
 	void DieProcess();
 
+	void ActivateBuff();
+
 	UFUNCTION()
 	void OnSeePlayer(APawn* SensedPawn);
 
+	void SetAllParams(int health, int damage, float attackInterval);
+
 	UFUNCTION()
 	bool IsTargetInFront(float AngleThresholdDegrees) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Combat|Fists")
+	void EnableFistCollision(bool bEnable);
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
 	class UPawnSensingComponent* PawnSensingComponent;
 
 };
-
