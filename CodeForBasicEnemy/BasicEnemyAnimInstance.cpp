e48878891dce723634b7a07daeaf61be557b828c
AmirGairatov <amircagay@gmail.com>
2026-01-03 00:26:31 +0500

    Update BasicEnemyAnimInstance.cpp

--- CodeForBasicEnemy/BasicEnemyAnimInstance.cpp
+++ CodeForBasicEnemy/BasicEnemyAnimInstance.cpp
@@ -1,3 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "BasicEnemyAnimInstance.h"
 #include "BasicEnemy.h"
 
@@ -5,10 +8,22 @@ void UBasicEnemyAnimInstance::OnStateAnimationEnds()
 {
 	if (State == EBasicEnemyState::Spawn)
 	{
+		auto enemy = Cast<ABasicEnemy>(GetOwningActor());
+		if (enemy)
+		{
+			enemy->bAreHit = false;	
+		}
 		State = EBasicEnemyState::Locomotion;
+
 	}
 	if (State == EBasicEnemyState::Attack || State == EBasicEnemyState::AttackUP || State == EBasicEnemyState::HeavyAttack)
 	{
+		auto enemy = Cast<ABasicEnemy>(GetOwningActor());
+		if (enemy)
+		{
+			enemy->bIsInAttackRecover = true;
+			enemy->_AttackRecoverCountingDown = enemy->AttackRecoverInterval;
+		}
 		State = EBasicEnemyState::Locomotion;
 	}
 	else
@@ -31,5 +46,13 @@ void UBasicEnemyAnimInstance::OnStateAnimationEnds()
 		}
 		
 	}
+}
 
+void UBasicEnemyAnimInstance::DamageDealControl(bool wtf)
+{
+	auto enemy = Cast<ABasicEnemy>(GetOwningActor());
+	if (enemy)
+	{
+		enemy->EnableFistCollision(wtf);
+	}
 }
